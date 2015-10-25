e438e1715f4f73cefce81fb3dce19e4973aba38b
akshayangadi <akshayangadi@gmail.com>
2015-10-26 00:12:09 +0530

    Create matrixInverse.R
    Programming assignment 2 by Akshay Angadi

---
+++ matrixInverse.R
@@ -0,0 +1,46 @@
+makeCacheMatrix <- function(x=matrix())
+{
+  n <- NULL
+  
+  set <- function(y)
+  {
+    x <<- y
+    n <<- NULL
+  }
+  
+  get <- function()
+  {
+    x
+  }
+  
+  setMat <- function(solve1)
+  {
+    n <<-solve1
+  }
+  
+  getMat <- function()
+  {
+    n
+  }
+  
+  list(set = set, get = get, setMat = setMat , getMat = getMat)
+}
+
+cacheSolve <- function(y = matrix(), ...)
+{
+  x<-as.data.frame(t(y))
+  n <- x$getMat()
+  if(!is.null(n))
+  {
+    message("Cache data being fetched")
+    return(n)
+  }
+  
+  mat <- x$get()
+  mat1<-as.matrix(mat)
+  n <- solve(mat1)
+  x$setMat(n)
+  
+  n
+
+}
